059cf552db78b2a3c305f19a43ba75ce43fb5d85
RomanJodlowski <75018677+RomanJodlowski@users.noreply.github.com>
2023-11-22 16:44:22 +0000

    n

---
+++ Testing Technologies & Programming/Week01/ArrowFunctions/Q4.js
@@ -0,0 +1,10 @@
+// traditional function
+// function nameAge(name, age) {
+//     console.log("Hello " + name);
+//     console.log("You are " + age + " years old");
+//   }
+
+// an arrow function
+let nameAge = (name, age) => "Hello " + name + " You are " + age + " years old"
+
+  console.log(nameAge("Roman", 53));
