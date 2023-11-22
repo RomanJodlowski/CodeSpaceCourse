7ceb815d8abf221a95adcea5b5e15c38dad42317
RomanJodlowski <75018677+RomanJodlowski@users.noreply.github.com>
2023-11-22 15:31:37 +0000

    n

--- Testing Technologies & Programming/Week01/ArrowFunctions/Q1.js
+++ Testing Technologies & Programming/Week01/ArrowFunctions/Q1.js
@@ -1,7 +1,11 @@
-function greed(name)
-{
-    return"Hello, ny name is " + name;
-}
+// traditional function
+// function greed(name)
+// {
+//     return"Hello, ny name is " + name;
+// }
+
+// anonimus function simplif 
+let greed = name => "Hello, my name is " + name;
 
 console.log("John");
 console.log("James");
