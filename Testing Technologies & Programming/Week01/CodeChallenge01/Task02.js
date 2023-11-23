44b58afeebd8a1d8a756f6ca41cc912df82dd464
roman <jodlowski.roman@gmail.com>
2023-11-23 00:26:29 +0000

    n

--- Testing Technologies & Programming/Week01/CodeChallenge01/Task02.js
+++ Testing Technologies & Programming/Week01/CodeChallenge01/Task02.js
@@ -1,12 +1,11 @@
 // Write a function expression called reverseArray(). It should accept an array as a single argument. 
 // It should return a reversed array and it should work for any data type.
 
-function reverseString(name) {
-    let splitArray = name.split("");
-    let reverseArray = splitArray.reverse();
-    let joinArray = reverseArray.join("");
-    return joinArray;
-    }
+function reverseArray() { 
+const stringReverse = ["I", " like", " JavaScript"].reverse(),
+      numbergReverse = [1, 2, 3, 4, 5].reverse();
+
+return [stringReverse, numbergReverse];
+}
     
-    console.log(reverseString(1, 2, 3, 4, 5));
-    console.log(reverseString("JavaScript", "like", "I"));
+    console.log(reverseArray().reverse());
