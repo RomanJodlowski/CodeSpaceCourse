9a081edee473c66c9d81c82a24cf297a9c9e3edb
Roman <75018677+RomanJodlowski@users.noreply.github.com>
2024-01-10 11:05:01 +0000

    Update secretNumber.py

--- Testing Technologies & Programming/SecretNumberGame/secretNumber.py
+++ Testing Technologies & Programming/SecretNumberGame/secretNumber.py
@@ -1,5 +1,18 @@
 import random
 
+
+# function to validate the user input if it is the integer
+def inputNumber(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("Must be an integer! Try again.")
+            continue
+        else:
+            return userInput
+
+
 # Generate the random number between 1 and 20
 secretNumber = random.randint(1, 20)
 
@@ -7,13 +20,16 @@ print("Welcome to the Secret Number Game!")
 
 while True:
     # Take the user input
-    user = int(input("Enter your number (between 1 and 20): "))
-
-    # Compare the numbers
-    if user == secretNumber:
-        print("You answered is", user, ". This is the correct answer!\nGame Over!")
-        break
-    elif user < secretNumber:
-        print("You answered is", user, ". The correct answer is higher.")
+    user = inputNumber("Enter your number (between 1 and 20): ")
+    if user > 20 or user < 1:
+        print('The range is between 1 and 20!')
     else:
-        print("You answered is", user, ". The correct answer is lower.")
+        # Compare the numbers
+        if user == secretNumber:
+            print("You answered is", user, ". This is the correct answer!\nGame Over!")
+            break
+        elif user < secretNumber:
+            print("You answered is", user, ". The correct answer is higher.")
+        else:
+            print("You answered is", user, ". The correct answer is lower.")
+
